52ddf2369a51712c5e77c2c0207f478eab84d023
Liberhaim <freez236@rambler.ru>
2021-03-08 17:30:16 +0300

    add Lesson_2

--- Python/Lesson_1/les1_2.py
+++
@@ -1,6 +0,0 @@
-time_in_sec = int(input("Введите время в секундах: "))
-hour = time_in_sec // 3600
-minute = (time_in_sec // 60) % 60
-sec = time_in_sec % 60
-
-print(" time is %02d:%02d:%02d" % (hour, minute, sec))
--- Python/Lesson_1/les1_3.py
+++
@@ -1,3 +0,0 @@
-data = float(input("Введите число n: "))
-data = data + (data * 11) + (data * 111)
-print(" Сумма чисел n составляет: ", data)
